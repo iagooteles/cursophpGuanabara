b261da1c39ddb90dd636eea51869002f19b92b82
Iago Teles <iagooteles@gmail.com>
2024-12-13 20:18:12 -0300

    finished caixaEletronico

--- caixaEletronico/index.php
+++ caixaEletronico/index.php
@@ -26,23 +26,33 @@
         $resto %= 10;
 
         $tot5 = floor($resto / 5);
-        $resto %= 5;
-    
+        $resto %= 5;    
     ?>
     <main>
         <h1>Caixa Eletrônico</h1>
         <h2>No caixa só há notas de R$100 R$50 R$10 e R$5 disponíveis.</h2>
 
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="seg">Qual o Valor total que deseja sacar?</label>
-            <input type="number" name="seg" id="seg" min="0" step="1" value="<?=$total_segundos?>" required>
+            <label for="seg">Qual o Valor total que deseja sacar? (R$)<sup>*</sup></label>
+            <input type="number" name="saque" id="saque" step="5" value="<?=$saque?>" required>
 
-            <input type="submit" value="Calcular">
+            <p style="font-size: 0.7em;">Notas disponíveis: R$100, R$50, R$10 e R$5</p>
+
+            <input type="submit" value="Sacar">
         </form>
     </main>
 
     <section>
-        <h2>Totalizando tudo</h2>
+        <h2>Saque de R$<?=number_format($saque, 2, "," , ".")?> realizado</h2>
+
+        <p>O caixa eletrônico vai te entregar as seguintes notas:</p>
+
+        <ul>
+            <li><img src="./images/100-reais.jpg" alt="nota de 100" class="nota"> x<?=$tot100?></li>
+            <li><img src="./images/50-reais.jpg" alt="nota de 50" class="nota"> x<?=$tot50?></li>
+            <li><img src="./images/10-reais.jpg" alt="nota de 10" class="nota"> x<?=$tot10?></li>
+            <li>5<img src="./images/5-reais.jpg" alt="nota de 5" class="nota"> x<?=$tot5?></li>
+        </ul>
 
     </section>
 
